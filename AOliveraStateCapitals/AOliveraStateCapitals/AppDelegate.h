0bb6337bc2aa3c05b80efec4a4bdbe05cd96a29e
Andrew Olivera <oliveraandrew@gmail.com>
2015-03-06 23:36:37 -0800

    state capitals assignment
    march 6 2015, just created new project.

---
+++ AOliveraStateCapitals/AOliveraStateCapitals/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AOliveraStateCapitals
+//
+//  Created by Andrew Olivera on 3/6/15.
+//  Copyright (c) 2015 sbcc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AOliveraStateCapitals/AOliveraStateCapitals/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AOliveraStateCapitals
+//
+//  Created by Andrew Olivera on 3/6/15.
+//  Copyright (c) 2015 sbcc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
